840e6268861ff3f64cef5be4dba3da0311115fe7
Marcin <marcin.mm25@gmail.com>
2025-06-01 12:35:38 +0200

    aktualizacja linków

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,4 +3,7 @@ from frontend import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
