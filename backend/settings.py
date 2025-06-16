caf290b4d3347ce769741796ddff31f0cfff34f9
Marcin <marcin.mm25@gmail.com>
2025-06-16 23:42:31 +0200

    update settings

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 INSTALLED_APPS = [
